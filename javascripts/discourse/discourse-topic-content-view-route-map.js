6cf1dab94b2a0dde4a008872fc70b6a81e72e3b1
denvergeeks <jen@denvergeeks.com>
2026-03-09 14:09:11 -0600

    Delete javascripts/discourse/discourse-topic-content-view-route-map.js

--- javascripts/discourse/discourse-topic-content-view-route-map.js
+++
@@ -1,8 +0,0 @@
-export default {
-  resource: "root",
-  path: "/",
-  map() {
-    this.route("topic-content-show", { path: "/tc/:slug/:id" });
-    this.route("topic-content-show", { path: "/tc/:id" });
-  },
-};
