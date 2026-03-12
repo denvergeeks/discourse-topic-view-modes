4a3c084cd58a0d1a97e786ce32f35d8c77943554
denvergeeks <jen@denvergeeks.com>
2026-03-12 00:10:32 -0600

    Delete assets/javascripts/discourse/routes/admin-plugins-topic-content-view.js

--- assets/javascripts/discourse/routes/admin-plugins-topic-content-view.js
+++
@@ -1,5 +0,0 @@
-import Route from "@ember/routing/route";
-
-// Data loading is handled inside the AdminPluginsTopicContentView Glimmer
-// component itself, so this route is intentionally minimal.
-export default class AdminPluginsTopicContentViewRoute extends Route {}
