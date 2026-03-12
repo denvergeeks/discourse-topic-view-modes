a5a6b70e874d1643f440ed59be9a087518ccc0f0
denvergeeks <jen@denvergeeks.com>
2026-03-11 18:07:21 -0600

    Remove custom Ember route (no custom endpoint needed; use ?tcv= param on normal /t/ routes)

--- assets/javascripts/discourse/routes/topic-content-view.js
+++
@@ -1,8 +0,0 @@
-import Route from "@ember/routing/route";
-import { ajax } from "discourse/lib/ajax";
-
-export default class TopicContentViewRoute extends Route {
-  model(params) {
-    return ajax(`/t/${params.id}/tcv.json`);
-  }
-}
