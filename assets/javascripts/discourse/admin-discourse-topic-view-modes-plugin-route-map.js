f50a2d7271fc62728e72b408ad6b88c2df83f7b3
denvergeeks <jen@denvergeeks.com>
2026-03-18 02:56:06 -0600

    Update admin-discourse-topic-view-modes-plugin-route-map.js

--- assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js
+++ assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js
@@ -1,1 +1,7 @@
-
+export default {
+  resource: "admin.adminPlugins.show",
+  path: "/plugins",
+  map() {
+    this.route("discourse-topic-view-modes", { path: "discourse-topic-view-modes" });
+  },
+};
