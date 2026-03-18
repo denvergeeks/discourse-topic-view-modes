34d0abb3d033af99bdd58063760e993430ac3264
denvergeeks <jen@denvergeeks.com>
2026-03-18 03:56:18 -0600

    Update admin_controller.rb

--- app/controllers/topic_view_modes/admin_controller.rb
+++ app/controllers/topic_view_modes/admin_controller.rb
@@ -1,1 +1,35 @@
+# frozen_string_literal: true
 
+module TopicViewModes
+  class AdminController < ::Admin::AdminController
+    def index
+      modes = JSON.parse(SiteSetting.topic_view_modes_modes || "[]")
+      render json: { modes: modes }
+    rescue JSON::ParserError
+      render json: { modes: [] }
+    end
+
+    def update
+      body = JSON.parse(request.body.read)
+      modes = body["modes"]
+
+      raise Discourse::InvalidParameters unless modes.is_a?(Array)
+
+      sanitized = modes.map do |m|
+        {
+          "value"   => m["value"].to_s.strip,
+          "label"   => m["label"].to_s.strip,
+          "classes" => m["classes"].to_s.strip,
+          "css"     => m["css"].to_s.strip,
+          "enabled" => m["enabled"] == true,
+          "preset"  => m["preset"] == true
+        }
+      end
+
+      SiteSetting.topic_view_modes_modes = sanitized.to_json
+      render json: success_json
+    rescue JSON::ParserError
+      render json: failed_json, status: 422
+    end
+  end
+end
