bbb3180787969052a385efa3fd6cb637b0c16661
denvergeeks <jen@denvergeeks.com>
2026-03-18 04:26:56 -0600

    Fix route map: remove top-level path to fix plugin_id generate error

--- assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js
+++ assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js
@@ -1,7 +1,8 @@
 export default {
   resource: "admin.adminPlugins.show",
-  path: "/plugins",
   map() {
-    this.route("discourse-topic-view-modes", { path: "discourse-topic-view-modes" });
+    this.route("discourse-topic-view-modes", {
+      path: "discourse-topic-view-modes",
+    });
   },
 };
