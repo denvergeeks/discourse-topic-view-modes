8c7c9fabd65a4f285be8fb5cfdd50a1c28e3e42d
denvergeeks <jen@denvergeeks.com>
2026-03-18 02:15:54 -0600

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,27 +1,27 @@
 # frozen_string_literal: true
-# name: discourse-topic-content-view
-# about: Adds configurable CSS classes to body via ?tcv=MODE query param
-# version: 2.0.0
+# name: discourse-topic-view-modes
+# about: Adds configurable CSS classes to body via ?tvm=MODE query param
+# version: 3.0.0
 # authors: @denvergeeks
-# url: https://github.com/denvergeeks/discourse-topic-content-view
+# url: https://github.com/denvergeeks/discourse-topic-view-modes
 
-enabled_site_setting :topic_content_view_enabled
+enabled_site_setting :topic_view_modes_enabled
 
-register_asset "stylesheets/topic-content-view.scss", :desktop
+register_asset "stylesheets/topic-view-modes.scss", :desktop
 
-add_admin_route "topic_content_view.admin.title", "discourse-topic-content-view", use_new_show_route: true
+add_admin_route "topic_view_modes.admin.title", "discourse-topic-view-modes", use_new_show_route: true
 
 after_initialize do
-  module ::TopicContentView
-    PLUGIN_NAME = "discourse-topic-content-view"
+  module ::TopicViewModes
+    PLUGIN_NAME = "discourse-topic-view-modes"
   end
 
-  require_relative "app/controllers/topic_content_view/admin_controller"
+  require_relative "app/controllers/topic_view_modes/admin_controller"
 
   Discourse::Application.routes.prepend do
-    get "/admin/plugins/discourse-topic-content-view" => "topic_content_view/admin#index",
+    get "/admin/plugins/discourse-topic-view-modes" => "topic_view_modes/admin#index",
         constraints: StaffConstraint.new
-    put "/admin/plugins/discourse-topic-content-view" => "topic_content_view/admin#update",
+    put "/admin/plugins/discourse-topic-view-modes" => "topic_view_modes/admin#update",
         constraints: StaffConstraint.new
   end
 
