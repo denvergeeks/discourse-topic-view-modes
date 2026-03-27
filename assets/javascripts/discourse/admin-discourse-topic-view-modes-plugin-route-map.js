0e1b11e45d13dcc4b0bb45372f0a9b0a07aa2820
denvergeeks <jen@denvergeeks.com>
2026-03-27 00:50:34 -0600

    Delete assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js

--- assets/javascripts/discourse/admin-discourse-topic-view-modes-plugin-route-map.js
+++
@@ -1,9 +0,0 @@
-export default {
-  resource: "admin.adminPlugins.show",
-  path: "/plugins",
-  map() {
-    this.route("discourse-topic-view-modes", {
-      path: "discourse-topic-view-modes",
-    });
-  },
-};
