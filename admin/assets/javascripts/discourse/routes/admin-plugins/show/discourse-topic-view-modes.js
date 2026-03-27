381c335b2c699b9c53c57c7804332fc08c500af6
denvergeeks <jen@denvergeeks.com>
2026-03-27 13:35:39 -0600

    Create discourse-topic-view-modes.js

---
+++ admin/assets/javascripts/discourse/routes/admin-plugins/show/discourse-topic-view-modes.js
@@ -0,0 +1,5 @@
+import Route from "@ember/routing/route";
+
+export default class AdminPluginsShowDiscourseTopicViewModesRoute extends Route {
+  // Add model/afterModel if you need data; otherwise this can stay empty
+}
